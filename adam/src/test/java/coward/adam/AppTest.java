fc2a5d2e460a9a75c04346459c36f49cac19b483
ywsing <stupidsing@gmail.com>
2014-01-20 23:20:58 +0800

    use junit 4 instead of 3.x

--- adam/src/test/java/coward/adam/AppTest.java
+++ adam/src/test/java/coward/adam/AppTest.java
@@ -1,47 +1,32 @@
 package coward.adam;
 
+import static org.junit.Assert.assertEquals;
+
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.io.PrintStream;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest 
-    extends TestCase
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
 
     /**
      * Rigourous Test :-)
      */
-    public void testApp()
+    @Test
+    public void testApp() throws IOException
     {
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
 		PrintStream ps = new PrintStream(baos);
 		System.setOut(ps);
 		App.main(new String[0]);
 		ps.close();
-		String s = new String(baos.toByteArray());
+		String s = new String(baos.toByteArray(), "UTF-8");
 		assertEquals("Hello World!\r\n", s);
     }
+
 }
