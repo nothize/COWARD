e999fbe4b46b7440f17eb57b0342b1f7fe540cab
Nothize <nothize@gmail.com>
2014-02-27 16:25:38 +0800

    Introduce previous game state to main game loop

--- src/main/java/coward/big2/Big2Game.java
+++ src/main/java/coward/big2/Big2Game.java
@@ -3,12 +3,16 @@ package coward.big2;
 public class Big2Game {
 	public static void main(String[] args) {
 		Controller c = new Controller();
-		GameState gs = c.shuffle();
-		
-		int player = c.findFirstPlayer();
-		PlayerStrategy ps = new SimplePlayerStrategy();
-		while ( !c.endGame() ) {
-			c.playCards(gs, player, ps.play(new SimplePlayerView(gs, player)));
-		}
+		GameState pgs = null;
+		do {
+			GameState gs = c.shuffle();
+			
+			int player = c.findFirstPlayer(gs, pgs);
+			PlayerStrategy ps = new SimplePlayerStrategy();
+			while ( !c.endGame(gs) ) {
+				c.playCards(gs, player, ps.play(new SimplePlayerView(gs, player)));
+			}
+			pgs = gs;
+		} while ( c.playAgain() );
 	}
 }
