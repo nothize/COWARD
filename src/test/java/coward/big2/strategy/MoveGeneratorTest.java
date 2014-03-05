11b9fcb32237168c08081718bb908da5b8d76f90
ywsing <stupidsing@gmail.com>
2014-03-05 10:43:01 +0800

    comments

--- src/test/java/coward/big2/strategy/MoveGeneratorTest.java
+++ src/test/java/coward/big2/strategy/MoveGeneratorTest.java
@@ -16,16 +16,24 @@ public class MoveGeneratorTest {
 
 	@Test
 	public void test() {
+
+		// 2 single combos
 		assertEquals(2, moveGenerator.generate(new Hand(ImmutableSet.asSet( //
 				new Card(Suit.CLUB___, Rank.ACE__) //
 				, new Card(Suit.DIAMOND, Rank.N2___) //
 				))).size());
 
+		// 2 single combos
+		// 1 double combos
 		assertEquals(3, moveGenerator.generate(new Hand(ImmutableSet.asSet( //
 				new Card(Suit.CLUB___, Rank.ACE__) //
 				, new Card(Suit.DIAMOND, Rank.ACE__) //
 				))).size());
 
+		// 4 single combos
+		// 6 double combos
+		// 4 triple combos
+		// 1 quadruple combos
 		assertEquals(15, moveGenerator.generate(new Hand(ImmutableSet.asSet( //
 				new Card(Suit.CLUB___, Rank.ACE__) //
 				, new Card(Suit.DIAMOND, Rank.ACE__) //
