e0275ebad954f7a340df3421c2aa230e418aed3b
Nothize <nothize@gmail.com>
2014-05-18 17:16:38 +0800

    Add full house move

--- src/main/java/coward/big2/strategy/MoveGenerator.java
+++ src/main/java/coward/big2/strategy/MoveGenerator.java
@@ -4,6 +4,9 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 import com.google.common.collect.ArrayListMultimap;
 import com.google.common.collect.Multimap;
 
@@ -18,6 +21,7 @@ import coward.immutable.ImmutableSet;
  * TODO generate full-house (3-2), four of a kind (4-1), straight, flush, straight flush
  */
 public class MoveGenerator {
+	private static final Log log = LogFactory.getLog(MoveGenerator.class);
 
 	private ImmutableSet<Card> empty = new ImmutableSet<Card>();
 
@@ -25,15 +29,62 @@ public class MoveGenerator {
 		List<ImmutableSet<Card>> rankedCards = groupSameRanks(hand.getCards());
 		List<ImmutableSet<Card>> results = new ArrayList<>();
 
-		// Generate singles/pairs/triples/quadruples
-		for (ImmutableSet<Card> cards : rankedCards)
-			for (int i = 1; i <= cards.size(); i++)
-				permute(cards, i, results);
+		generateSameRank(rankedCards, results);
+		generateFullHouse(rankedCards, results);
 
 		return results;
 	}
 
-	private List<ImmutableSet<Card>> groupSameRanks(ImmutableSet<Card> cards) {
+	public void generateFullHouse(List<ImmutableSet<Card>> rankedCards,
+			List<ImmutableSet<Card>> results) {
+		// Find ranks with at least 3 pairs and 2 pairs
+		List<ImmutableSet<Card>> threess = new ArrayList<>();
+		List<ImmutableSet<Card>> twoss = new ArrayList<>();
+		for (ImmutableSet<Card> cards : rankedCards) {
+			if ( cards.size() >= 2 ) {
+				twoss.add(cards);
+				if ( cards.size() >= 3 ) {
+					threess.add(cards);
+				}
+			}
+		}
+		log.debug("Threess: " + threess);
+		log.debug("Twoss:" + twoss);
+		
+		// The minimum set of full house must contains minimum 3 pairs and 2 pairs thus total 1 threes and 2 twos.
+		if ( threess.size() >= 1 && twoss.size() >= 2 ) {
+			List<ImmutableSet<Card>> threes = new ArrayList<>();
+			// Generate the permutation set of all threes
+			for (ImmutableSet<Card> cards : threess) {
+				permute(cards, 3, threes);
+			}
+			log.debug("threes: " + threes);
+			List<ImmutableSet<Card>> twos = new ArrayList<>();
+			// Generate the permutation set of all twos
+			for (ImmutableSet<Card> cards : twoss) {
+				permute(cards, 2, twos);
+			}
+			log.debug("twos: " + twos);
+			// For each three, join with all two
+			for (ImmutableSet<Card> cards3 : threes) {
+				for (ImmutableSet<Card> cards2 : twos) {
+					if ( cards3.iterator().next().getRank() != cards2.iterator().next().getRank() ) {
+						results.add(cards3.addAll(cards2));
+					}
+				}
+			}
+		}
+	}
+
+	private void generateSameRank(List<ImmutableSet<Card>> rankedCards,
+			List<ImmutableSet<Card>> results) {
+		// Generate singles/pairs/triples/quadruples
+		for (ImmutableSet<Card> cards : rankedCards)
+			for (int i = 1; i <= cards.size(); i++)
+				permute(cards, i, results);
+	}
+
+	List<ImmutableSet<Card>> groupSameRanks(ImmutableSet<Card> cards) {
 		Multimap<Rank, Card> cardsByRank = ArrayListMultimap.create();
 
 		for (Card card : cards)
@@ -49,7 +100,7 @@ public class MoveGenerator {
 	 * Pick combinations of 'size' cards from first parameter, and add into
 	 * results parameter.
 	 */
-	private void permute(ImmutableSet<Card> cards, int size, List<ImmutableSet<Card>> results) {
+	void permute(ImmutableSet<Card> cards, int size, List<ImmutableSet<Card>> results) {
 		int diff = cards.size() - size;
 		if (diff >= 0)
 			permute(cards, diff, empty, results);
--- src/test/java/coward/big2/strategy/MoveGeneratorTest.java
+++ src/test/java/coward/big2/strategy/MoveGeneratorTest.java
@@ -2,6 +2,13 @@ package coward.big2.strategy;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.junit.Test;
 
 import coward.big2.Hand;
@@ -9,9 +16,10 @@ import coward.big2.card.Card;
 import coward.immutable.ImmutableSet;
 
 public class MoveGeneratorTest {
+	private static final Log log = LogFactory.getLog(MoveGeneratorTest.class);
 
 	private MoveGenerator moveGenerator = new MoveGenerator();
-
+	
 	@Test
 	public void test() {
 
@@ -38,5 +46,49 @@ public class MoveGeneratorTest {
 				, Card.HA //
 				, Card.SA //
 				))).size());
+	}
+
+	@Test
+	public void testPermute3() throws Exception {
+		List<ImmutableSet<Card>> results = new ArrayList<>();
+		moveGenerator.permute(ImmutableSet.asSet(Card.SA, Card.HA, Card.CA), 
+				3, results);
+		assertEquals(1, results.size());
+		ImmutableSet<Card> cards = results.get(0);
+		assertEquals(3, cards.size());
+		log.debug(cards);
+		Set<Card> cardSet = new HashSet<>();
+		for (Card card : cards) {
+			cardSet.add(card);
+		}
+		assertEquals(com.google.common.collect.ImmutableSet.of(Card.SA, Card.HA, Card.CA)
+				, cardSet);
+	}
+	
+	@Test
+	public void testFullHouse() throws Exception {
+		Hand hand = new Hand(ImmutableSet.asSet(
+				Card.CA, Card.DA, Card.SA, Card.HA
+				, Card.C2, Card.H2
+		));
+		List<ImmutableSet<Card>> rankedCards = moveGenerator.groupSameRanks(hand.getCards());
+		List<ImmutableSet<Card>> results = new ArrayList<>();
+
+		moveGenerator.generateFullHouse(rankedCards, results);
+		log.debug("result: " + results);
+		
+		assertEquals(4, results.size());
+
+		hand = new Hand(ImmutableSet.asSet(
+				Card.CA, Card.DA, Card.SA
+				, Card.C2, Card.H2, Card.S2
+		));
+		rankedCards = moveGenerator.groupSameRanks(hand.getCards());
+		results = new ArrayList<>();
+
+		moveGenerator.generateFullHouse(rankedCards, results);
+		log.debug("result: " + results);
+		
+		assertEquals(6, results.size());
 	}
 }
